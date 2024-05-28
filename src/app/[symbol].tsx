a163cffdc0de13a59b4bbdf498b3ad012aa26d86
Niti Chaudhary <nitic04@gmail.com>
2024-05-28 11:21:45 -0400

    Made stock details screen

---
+++ src/app/[symbol].tsx
@@ -0,0 +1,24 @@
+import { View, Text } from '@/src/components/Themed';
+import { Stack, useLocalSearchParams } from 'expo-router';
+import top5 from '@/assets/data/top5.json';
+import React from 'react';
+import StockListItem from '../components/StockListItem';
+
+const StockDetails = () => {
+    const { symbol } = useLocalSearchParams();
+
+    const stock = top5[symbol];
+
+    if (!stock) {
+        return <Text>Stock with symbol {symbol} could not be found.</Text>
+    }
+
+    return (
+        <View style={{ padding: 10 }}>
+            <Stack.Screen options={{ title: stock.symbol}} />
+            <StockListItem stock={stock}/>
+        </View>
+    )
+}
+
+export default StockDetails;
